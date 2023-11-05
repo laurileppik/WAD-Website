4d26ef178bf8760808fae78b5d951c05ec1ba47d
laurileppik <laurile02@gmail.com>
2023-11-05 17:38:30 +0200

    validated css

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,61 @@
-/** 
- * Fetch from online endpoint (URI)
-fetch('https://api.npoint.io/5c80200ef7e4b2ee9850')
-**/
+
+// Fetch from online endpoint (URI)
+/**fetch('https://api.npoint.io/5c80200ef7e4b2ee9850')
+.then((response) => response.json())
+.then(json => {
+    console.log(json);
+
+    const postContainer = document.getElementById('post-container');
+    // Loop through each post data in the JSON
+    json.forEach(postData => {
+        // Create a new post element
+        const postElement = document.createElement('div');
+        postElement.className = 'post';
+
+        // Create the poster element for each post
+        const poster = document.createElement('div');
+        poster.className = 'poster';
+
+        // Create the left part of the poster
+        const left = document.createElement('div');
+        left.className = 'left';
+        left.innerHTML = '<div class="image"><img src="res/images/blankpfp.webp" width="25" height="25" alt="My picture"></div>';
+        poster.appendChild(left);
+
+        // Create the right part of the poster with the post's creation time
+        const right = document.createElement('div');
+        right.className = 'right';
+        right.innerHTML = `<p>${postData.createTime}</p>`;
+        poster.appendChild(right);
+
+        // Append the poster to the post element
+        postElement.appendChild(poster);
+        if (postData.hasOwnProperty("image")) {
+            const imageUrl = postData.image;
+
+            // Create an image element and set its source
+            const imgElement = document.createElement('img');
+            imgElement.src = imageUrl;
+
+            // Append the image element to a container in your HTML
+            postElement.appendChild(imgElement);
+
+        }
+
+        // Create the post text element and set its content to the post's body
+        const postText = document.createElement('p');
+        postText.textContent = postData.body;
+        postElement.appendChild(postText);
+
+        // Create the like button for each post
+        const likeButton = document.createElement('div');
+        likeButton.className = 'like';
+        likeButton.innerHTML = '<img src="res/images/fblike.jpeg" width="25" height="25" alt="My picture">';
+        postElement.appendChild(likeButton);
+
+        postContainer.appendChild(postElement);
+    });
+});**/
 
 // Integrated JSON fetch
 fetch('res/json/myjson.json')
@@ -20,20 +74,6 @@ fetch('res/json/myjson.json')
             const poster = document.createElement('div');
             poster.className = 'poster';
 
-    
-            if (postData.hasOwnProperty("image")) {
-                const imageUrl = postData.image;
-
-                // Create an image element and set its source
-                const imgElement = document.createElement('img');
-                imgElement.src = imageUrl;
-
-                // Append the image element to a container in your HTML
-                postElement.appendChild(imgElement);
-
-            }
-            
-
             // Create the left part of the poster
             const left = document.createElement('div');
             left.className = 'left';
@@ -48,6 +88,17 @@ fetch('res/json/myjson.json')
 
             // Append the poster to the post element
             postElement.appendChild(poster);
+            if (postData.hasOwnProperty("image")) {
+                const imageUrl = postData.image;
+
+                // Create an image element and set its source
+                const imgElement = document.createElement('img');
+                imgElement.src = imageUrl;
+
+                // Append the image element to a container in your HTML
+                postElement.appendChild(imgElement);
+
+            }
 
             // Create the post text element and set its content to the post's body
             const postText = document.createElement('p');
